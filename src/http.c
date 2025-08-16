7846d6024a3818401917d6863777196837ecd2ec
Tinu205 <dazleroger404@gmail.com>
2025-08-16 16:04:56 +0530

    Added c file

--- src/http.c
+++ src/http.c
@@ -7,41 +7,62 @@
     - providing helper functions for Person A to call
 */
 
-#include <...>   // stdio, stdlib, string functions
+#include <string.h>
+#include "http.h"
 
-// 1. Define a struct to hold parsed request info
-//    - method (string)
-//    - path (string)
-//    - version (string)
-//    - headers (simple key-value array, optional at first)
-//    - body (optional)
 
-struct http_request {
-    // char *method;
-    // char *path;
-    // char *version;
-    // struct header { char *key; char *value; } headers[...];
-    // char *body;
+
+struct http_header{
+    char *key;
+    char *value;
 };
 
-/*
-    Function: parse_http_request
-    Input: raw HTTP request string (from server.c)
-    Output: filled http_request struct
-*/
-struct http_request parse_http_request(char *raw) {
-    // 1. Split raw request into lines
-    // 2. Parse request line (method, path, version)
-    // 3. Parse headers (optional at first)
-    // 4. Body (optional, can be empty)
-    // 5. Return request struct
+struct http_request {
+    char *method;
+    char *path;
+    char *version;
+    struct http_header * header;
+    int header_count;
+    //char *body;
+};
+
+
+void add_header(struct http_request *req,const char *key,const char *value){
+    req->header = realloc(req->header,(req->header_count+1)*sizeof(struct http_header));
+    req->header[req->header_count].key = strdup(key);
+    req->header[req->header_count].value = strdup(value);
+    req->header_count++;
 }
 
-/*
-    Function: free_http_request
-    Purpose: Free memory allocated during parsing
-*/
+
+struct http_request parse_http_request(char *raw) {
+    struct http_request request;
+    char * token = strtok(raw,"\r\n");
+    while(token){
+        request.method = strtok(token," ");
+        request.path = strtok(NULL," ");
+        request.version = strtok(NULL," ");
+    }
+    token = strtok(NULL,"\r\n");
+    while(token != NULL){
+        char * colon_pos = strchr(token,":");
+        if(colon_pos != NULL){
+            *colon_pos = '\0';
+            char *key = token;
+            char *value = colon_pos+1;
+            while(*value == ' ') value ++;
+            add_header(request,key,value);
+        }
+        token = strtok(NULL,"\r\n");
+    }
+    return request;
+}
+
+
 void free_http_request(struct http_request *req) {
-    // Free all strings/arrays allocated in parse_http_request
+    for(int i=0;i<req->header_count;i++){
+        free(req->header[i].key);
+        free(req->header[i].value);
+    }
 }
 
